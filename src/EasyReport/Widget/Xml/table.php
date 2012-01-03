08b6536829bf9fada1e65375ece09cc7d7c0622f
Iván Guardado <dev.ivangc@gmail.com>
2012-01-04 00:31:43 +0100

    Replaced php open short tags with the standard tag

--- src/EasyReport/Widget/Xml/table.php
+++ src/EasyReport/Widget/Xml/table.php
@@ -2,20 +2,20 @@
     <table:table-column table:style-name="Table1.A" table:number-columns-repeated="<?php echo count($this->getHeaderRow())?>"/>
         
         <table:table-row>
-            <?foreach($this->getHeaderRow() as $field):?>
+            <?php foreach($this->getHeaderRow() as $field):?>
             <table:table-cell table:style-name="Table1.A1" office:value-type="string">
                 <text:p text:style-name="Table_20_Contents"><?php echo $field?></text:p>
             </table:table-cell>
-            <?endforeach?>
+            <?php endforeach?>
         </table:table-row>
         
-        <?foreach($this->getData() as $row):?>
+        <?php foreach($this->getData() as $row):?>
         <table:table-row>
-            <?foreach($row as $cell):?>
+            <?php foreach($row as $cell):?>
                 <table:table-cell table:style-name="Table1.A1" office:value-type="string">
                     <text:p text:style-name="Table_20_Contents"><?php echo $cell?></text:p>
                 </table:table-cell>
-            <?endforeach?>
+            <?php endforeach?>
         </table:table-row>
-        <?endforeach?>
+        <?php endforeach?>
 </table:table>
